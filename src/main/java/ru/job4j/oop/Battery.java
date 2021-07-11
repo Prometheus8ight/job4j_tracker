58a799fbd6d1545e14191e10ee06a8e6078e58ed
Prometheus8ight <pointofview8*@gmai.com>
2021-07-11 11:07:32 +0300

    1.8. Состояние объекта [#167388 #176217]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -1,13 +1,6 @@
 package ru.job4j.oop;
 
 public class Battery {
-    public static void main(String[] args) {
-        Battery battery1 = new Battery(10);
-        Battery battery2 = new Battery(5);
-        battery1.exchange(battery2);
-        System.out.println("First load: " + battery1.load + ". Second battery load: " + battery2.load + ".");
-    }
-
     private int load;
 
     public Battery(int load) {
@@ -15,7 +8,14 @@ public class Battery {
     }
 
     public void exchange(Battery another){
-        another.load += 1;
-        this.load -= 1;
+        another.load += this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery battery1 = new Battery(10);
+        Battery battery2 = new Battery(5);
+        battery1.exchange(battery2);
+        System.out.println("First load: " + battery1.load + ". Second battery load: " + battery2.load + ".");
     }
 }
