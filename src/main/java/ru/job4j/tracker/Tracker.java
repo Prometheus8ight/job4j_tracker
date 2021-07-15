ede0cc28a6addeb624d3b4f0470085bcaf3ec9e0
Prometheus8ight <pointofview8*@gmai.com>
2021-07-15 11:14:05 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #177801]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -35,7 +35,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            System.arraycopy(items, index + 1, items, index, items.length - index - 1);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
             return true;
