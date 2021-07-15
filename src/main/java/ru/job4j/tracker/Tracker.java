19e83e04283c31191cf9127db496a1b20a0316cc
Prometheus8ight <pointofview8*@gmai.com>
2021-07-15 13:47:38 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #177783]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -24,8 +24,8 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
+            item.setId(id);
             items[index] = item;
-            items[index].setId(id);
             return true;
         }
         return false;
