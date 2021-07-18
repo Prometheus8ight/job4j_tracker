bac900a21381bcd9abb1ae5ee0fdf5070b96210a
Prometheus8ight <pointofview8*@gmai.com>
2021-07-18 23:10:35 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int numberOfPassengers) {
+
+    }
+
+    @Override
+    public int tankUp(int fuel) {
+        return 0;
+    }
+}
